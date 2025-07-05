01cd829d5c8613883b5d3a169350ce36492106c4
17630921248 <1245634367@qq.com>
2025-07-05 09:25:13 +0800

    feature: 修改运行和打包命令

---
+++ dev.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: 17630921248 1245634367@qq.com
+ * @Date: 2025-07-05 08:59:33
+ * @LastEditors: 17630921248 1245634367@qq.com
+ * @LastEditTime: 2025-07-05 08:59:36
+ * @FilePath: \open-reobot-ui\dev.js
+ * @Description: Fuck Bug
+ * 微信：lizx2066
+ */
+const { execSync } = require('child_process')
+
+if (process.platform === 'win32') {
+  execSync('chcp 65001', { stdio: 'inherit' })
+}
+
+execSync('electron-vite dev', { stdio: 'inherit' })
